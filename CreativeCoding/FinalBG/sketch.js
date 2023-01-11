6ef9af5e5233b1696f1e5514412cdabe7a09b50e
Gburraway <gracieburraway@icloud.com>
2023-01-11 09:17:58 -0500

    FInal

---
+++ CreativeCoding/FinalBG/sketch.js
@@ -0,0 +1,28 @@
+let img;
+
+function preload (){
+  img = loadImage ('assets/Balloon.png');
+}
+function setup() {//runs once
+createCanvas (windowWidth, windowWidth);//creates a canvas 720 by 720 pixels
+
+
+
+}
+
+function draw() {//runs in a loop
+  fill ('lavender');//Shape with RGB color
+  strokeWeight (1);//assigns stroke weight
+var num = 6;
+var sideLen = windowWidth/num;
+for (var y = 0; y < windowWidth; y = y + sideLen) {
+for (var x = 0; x < windowWidth; x = x + sideLen) {
+  image (img, x, y, windowWidth/num, windowWidth/num);
+quad (x,y,
+      x + sideLen, y,
+      x + sideLen,y + sideLen,
+      x,y + sideLen);
+
+}
+}
+}
